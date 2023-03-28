d23a7fe289c5fad50b34970993e8d913ea2d385f
Bruce <bruce.hillsworth@gmail.com>
2023-03-28 16:00:59 -0600

    Fixed css pointer for .activated-heart.  Added eventListener's to like-glyph elements and a server call

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
+const likeGlyph = document.querySelectorAll("span.like-glyph");
+const modal = document.querySelector("#modal");
 
-
-
+likeGlyph.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    mimicServerCall()
+      .then((response) => {
+        if (element.className === "like-glyph") {
+          element.className = "activated-heart";
+        } else {
+          element.className = "like-glyph";
+        }
+      })
+      .catch((response) => {
+        modal.className = "not-hidden";
+        setTimeout(() => (modal.className = "hidden"), 3000);
+      });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
